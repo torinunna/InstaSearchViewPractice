14ae385e92d835f988356625bab3d48a13371df3
torinunna <torinunna9588@gmail.com>
2023-05-08 22:37:28 +0900

    Initial Commit

---
+++ InstaSearchViewPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaSearchViewPractice
+//
+//  Created by YUJIN KWON on 2023/05/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
