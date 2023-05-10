573526c6613662327093bb883ec13d4a51346771
torinunna <torinunna9588@gmail.com>
2023-05-10 15:03:14 +0900

    create SearchVC

---
+++ InstaSearchViewPractice/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  InstaSearchViewPractice
+//
+//  Created by YUJIN KWON on 2023/05/10.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+
+}
