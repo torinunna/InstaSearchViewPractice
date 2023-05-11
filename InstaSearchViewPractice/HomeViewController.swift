2e5b484926400dbd27ba0954e29686340e831543
torinunna <torinunna9588@gmail.com>
2023-05-11 16:13:38 +0900

    create HomeVC

---
+++ InstaSearchViewPractice/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  InstaSearchViewPractice
+//
+//  Created by YUJIN KWON on 2023/05/11.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
